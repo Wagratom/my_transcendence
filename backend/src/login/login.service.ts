ecb03a68bb3f1e6a9c59090a2b7fd585b8ba5669
Wagratom <wagratom111@gmail.com>
2024-07-27 15:38:41 -0300

    feat: add new validation rule

--- backend/src/login/login.service.ts
+++ backend/src/login/login.service.ts
@@ -1,8 +1,16 @@
-import { Injectable } from '@nestjs/common';
+import {
+  ConflictException,
+  Injectable,
+  NotFoundException,
+} from '@nestjs/common';
 import LoginServiceInterface from './interface/login.service.interface';
-import { ChangePasswordDto, ForgotPasswordDto, RegisterUserDto, UserCredentials } from './dto/login.dto';
+import {
+  ChangePasswordDto,
+  ForgotPasswordDto,
+  RegisterUserDto,
+  UserCredentials,
+} from './dto/login.dto';
 import UserResponseDto from './dto/login.response.dto';
-import LoginRepositoryInterface from './interface/prisma.service.interface';
 import LoginRepository from './login.repository';
 
 @Injectable()
@@ -10,7 +18,11 @@ export class LoginService implements LoginServiceInterface {
   constructor(private readonly prismaService: LoginRepository) {}
 
   async login(userData: UserCredentials): Promise<UserResponseDto> {
-    return this.prismaService.login(userData);
+    let user = await this.prismaService.findUser(userData);
+    if (!user) {
+      throw new NotFoundException('User not found');
+    }
+    return  new UserResponseDto(await this.prismaService.login(userData));
   }
 
   async logout(userData: UserCredentials): Promise<boolean> {
@@ -18,7 +30,11 @@ export class LoginService implements LoginServiceInterface {
   }
 
   async register(userData: RegisterUserDto): Promise<UserResponseDto> {
-    return this.prismaService.register(userData);
+    let user = this.prismaService.findUser(userData)
+    if (user) {
+      throw new ConflictException('User already exist')
+    }
+    return new  UserResponseDto(await this.prismaService.register(userData));
   }
 
   async forgotPassword(userData: ForgotPasswordDto): Promise<UserResponseDto> {
--- backend/src/main.ts
+++ backend/src/main.ts
@@ -5,7 +5,9 @@ import { ValidationPipe } from '@nestjs/common';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors();
-  app.useGlobalPipes(new ValidationPipe({ whitelist: true, forbidNonWhitelisted: true }));
+  app.useGlobalPipes(
+    new ValidationPipe({ whitelist: true, forbidNonWhitelisted: true }),
+  );
   const port = process.env.PORT || 5000;
   await app.listen(3000);
 }
