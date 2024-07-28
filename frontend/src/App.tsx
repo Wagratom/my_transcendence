898377636abfd3cf707aa931d9a7818219d477e4
Wagratom <wagratom111@gmail.com>
2024-07-27 23:01:15 -0300

    remove fake login

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,7 +2,6 @@ import './index.css';
 import { Login } from './components/LoginPage/Login';
 import InicialPage from './components/InitialPage/InitialPage';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import LoginFake from './components/LoginPage/LoginFake';
 import Game from './components/GamePage/Game/Game';
 import GameWW from './components/GamePage/Game/GameWW';
 
@@ -12,7 +11,6 @@ export default function App() {
 			<BrowserRouter>
 				<Routes>
 					<Route path="/" element={<Login />} />
-					<Route path="/fake" element={<LoginFake />} />
 					<Route path="/game/" element={<InicialPage />}>
 						<Route index element={<Game />} />
 						<Route path="pong/:room" element={<GameWW />} />
