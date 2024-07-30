2371b067743da75bf21d83cd3e5cc5be890ce4b2
Wagratom <wagratom111@gmail.com>
2024-07-30 15:02:19 -0300

    feat: add requesition for getProfile

--- frontend/src/components/InitialPage/InitialPage.tsx
+++ frontend/src/components/InitialPage/InitialPage.tsx
@@ -1,17 +1,32 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import { Outlet } from 'react-router-dom';
 import { UserData, t_dataUser } from '../Contexts/Contexts';
+import axios from 'axios';
 
 export default function InicialPage() {
 	// Function called to load the context with user information before rendering the game page
-
-	const [infoUser, setGetInfoUser] = useState<t_dataUser>({} as t_dataUser);
-
-
+	const [infoUser, setInfoUser] = useState<t_dataUser>({} as t_dataUser);
+	useEffect(() => {
+		axios.get('https://localhost/api/users/getProfile', {
+			timeout: 5000,
+			withCredentials: true
+		}).then((response) => {
+			response.data.authorized = true
+			setInfoUser(response.data)
+		}).catch((error) => {
+			if (error.status === 401) {
+				let user : t_dataUser = {} as t_dataUser
+				user.authorized = false
+				setInfoUser(user)
+			}
+		})
+	}, []);
 	return (
-		<UserData.Provider value={{ user: infoUser, updateDataUser: () => {} }}>
-			<Outlet />
-		</UserData.Provider>
+		<>
+			<UserData.Provider value={{ user: infoUser, updateDataUser: () => { } }}>
+				<Outlet />
+			</UserData.Provider>
+		</>
 
 	);
 }
