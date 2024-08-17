952ef890a5c4ca0ed6f09996b498c427edd919a0
Wagratom <wagratom111@gmail.com>
2024-08-17 10:27:46 -0300

    feat: update logic to add a friend

--- backend/src/users/users.repository.ts
+++ backend/src/users/users.repository.ts
@@ -1,7 +1,7 @@
 import { PrismaService } from "src/PrismaDB/prisma.connect";
 import UserRepositoryInterface from "./interface/user.repository.interface";
-import { User } from "@prisma/client";
-import { Injectable } from "@nestjs/common";
+import { Friendship, User } from "@prisma/client";
+import { Injectable, InternalServerErrorException } from "@nestjs/common";
 import { UsersResponseDto } from "./dto/users.response.dto";
 import { StatusFriendship } from "@prisma/client";
 
@@ -60,7 +60,6 @@ export default class UsersRepository implements UserRepositoryInterface {
 		return friends;
 	}
 
-
 	async getFriendRequests(username: string): Promise<User[]> {
 		const user = await this.prismaService.user.findUnique({
 			where: {
@@ -92,7 +91,6 @@ export default class UsersRepository implements UserRepositoryInterface {
 
 	}
 
-
 	async updateUser(updateData: { username: string, nickname?: string; photo?: string }): Promise<UsersResponseDto> {
 		let user = await this.prismaService.user.update({
 			where: {
@@ -106,14 +104,54 @@ export default class UsersRepository implements UserRepositoryInterface {
 		return new UsersResponseDto(user);
 	}
 
-	async addFriend(sendID: number, friendID: number): Promise<void> {
-		await this.prismaService.friendship.create({
-			data: {
-				sentRequestsId: sendID,
-				receivedRequestsId: friendID,
-				status: StatusFriendship.PENDING,
+	async getFriendship(sendID: number, friendID: number): Promise<Friendship | null> {
+		return await this.prismaService.friendship.findFirst({
+			where: {
+				OR: [
+					{
+						sentRequestsId: sendID,
+						receivedRequestsId: friendID
+					},
+					{
+					sentRequestsId: friendID,
+					receivedRequestsId: sendID
+					}
+				]
 			}
-		});
+		})
 	}
 
+	async createNewFriendship(sendID: number, friendID: number): Promise<void>{
+		await this.prismaService.friendship.create({
+			data: {
+					sentRequestsId: sendID,
+					receivedRequestsId: friendID,
+					status: StatusFriendship.ACCEPTED
+				}
+		})
+	}
+
+	async updateStatusFriendship(id: number, status: StatusFriendship): Promise<void>{
+		await this.prismaService.friendship.update({
+			where: {
+				id: id
+			},
+			data: {
+				status: status
+			}
+		})
+	}
+
+	async addFriend(sendID: number, friendID: number): Promise<void> {
+		try {
+			let friendship = await this.getFriendship(sendID, friendID);
+			if (friendship) {
+				await this.updateStatusFriendship(friendship.id, StatusFriendship.ACCEPTED);
+			} else {
+				await this.createNewFriendship(sendID, friendID);
+			}
+		} catch (error) {
+			throw new InternalServerErrorException("Error: Unable to add friend")
+		}
+	}
 }
